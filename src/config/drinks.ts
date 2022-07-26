83222f82d5457aafe2431a9e6c85d2661b31a2da
JT Salisbury <crazyscouter1@gmail.com>
2022-07-26 15:43:50 -0400

    add drinks

--- src/config/drinks.ts
+++ src/config/drinks.ts
@@ -1,5 +1,5 @@
 import { Drink } from "../models/Drink";
-import { VODKA, ORANGE_JUICE } from "./ingredients";
+import { VODKA, ORANGE_JUICE, RUM, COKE, LIME_JUICE, TEQUILA, CRANBERRY } from "./ingredients";
 
 const Drinks: Drink[] = [
     {
@@ -9,6 +9,40 @@ const Drinks: Drink[] = [
         ingredients: [
             {
                 base: VODKA,
+                ounces: 2
+            },
+            {
+                base: ORANGE_JUICE,
+                ounces: 3
+            }
+        ]
+    },
+    {
+        id: 1,
+        name: 'Rum and Coke',
+        image: 'rum_coke.jpg',
+        ingredients: [
+            {
+                base: RUM,
+                ounces: 1.6
+            },
+            {
+                base: COKE,
+                ounces: 4
+            },
+            {
+                base: LIME_JUICE,
+                ounces: 0.6
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: 'Tequila Sunrise',
+        image: 'tequila_sunrise.jpg',
+        ingredients: [
+            {
+                base: TEQUILA,
                 ounces: 1.5
             },
             {
@@ -16,7 +50,122 @@ const Drinks: Drink[] = [
                 ounces: 3
             }
         ]
-    }
+    },
+    {
+        id: 3,
+        name: 'Long Island',
+        image: 'long_island.jpg',
+        ingredients: [
+            {
+                base: VODKA,
+                ounces: 0.5
+            },
+            {
+                base: RUM,
+                ounces: 0.5
+            },
+            {
+                base: TEQUILA,
+                ounces: 0.5
+            },
+            {
+                base: LIME_JUICE,
+                ounces: 1
+            },
+            {
+                base: COKE,
+                ounces: 1
+            } // TODO: add gin, triple sec
+        ]
+    },
+    {
+        id: 4,
+        name: 'Margarita',
+        image: 'marg.jpg',
+        ingredients: [
+            {
+                base: TEQUILA,
+                ounces: 1.5
+            },
+            {
+                base: LIME_JUICE,
+                ounces: 0.75
+            }
+        ] // TODO: add triple sec 1 oz
+    },
+    {
+        id: 5,
+        name: 'Orange Margarita',
+        image: 'orange_marg.jpg',
+        ingredients: [
+            {
+                base: TEQUILA,
+                ounces: 1.5
+            },
+            {
+                base: ORANGE_JUICE,
+                ounces: 1.5
+            },
+            {
+                base: LIME_JUICE,
+                ounces: 1
+            }
+        ] // TODO: add triple sec 1 oz
+    },
+    {
+        id: 6,
+        name: 'Rum and Cranberry',
+        image: 'rum_cran.jpg',
+        ingredients: [
+            {
+                base: RUM,
+                ounces: 1.5
+            },
+            {
+                base: CRANBERRY,
+                ounces: 1.5
+            },
+            {
+                base: LIME_JUICE,
+                ounces: 0.5
+            }
+        ] // TODO: add simple syrup
+    },
+    {
+        id: 7,
+        name: 'Vodka Cranberry',
+        image: 'vodka_cran.jpg',
+        ingredients: [
+            {
+                base: VODKA,
+                ounces: 1
+            },
+            {
+                base: CRANBERRY,
+                ounces: 3
+            }
+        ] // TODO: add simple syrup
+    },
+    {
+        id: 8,
+        name: 'Cranberry Margarita',
+        image: 'cranberry_marg.jpg',
+        ingredients: [
+            {
+                base: TEQUILA,
+                ounces: 1.5
+            },
+            {
+                base: CRANBERRY,
+                ounces: 1.5
+            },
+            {
+                base: LIME_JUICE,
+                ounces: 0.5
+            }
+
+        ] // TODO: add simple syrup 0.5, 1 oz cointreau
+    },
 ]
 
 export default Drinks;
--- src/config/ingredients.ts
+++ src/config/ingredients.ts
@@ -7,6 +7,24 @@ const VODKA: BaseIngredient = {
     pump_id: 0
 }
 
+const TEQUILA: BaseIngredient = {
+    id: 2,
+    name: 'Tequila',
+    image: '',
+    pump_id: 2
+}
+
+const RUM: BaseIngredient = {
+    id: 3,
+    name: 'Rum',
+    image: '',
+    pump_id: 3
+}
+
+
+
+
+
 const ORANGE_JUICE: BaseIngredient = {
     id: 1,
     name: 'Orange Juice',
@@ -14,6 +32,27 @@ const ORANGE_JUICE: BaseIngredient = {
     pump_id: 1
 }
 
-const Ingredients = [VODKA, ORANGE_JUICE];
+const LIME_JUICE: BaseIngredient = {
+    id: 4,
+    name: 'Lime Juice',
+    image: '',
+    pump_id: 4
+}
 
-export { VODKA, ORANGE_JUICE, Ingredients };
+const COKE: BaseIngredient = {
+    id: 5,
+    name: 'Coke',
+    image: '',
+    pump_id: 5
+}
+
+const CRANBERRY: BaseIngredient = {
+    id: 6,
+    name: 'Cranberry',
+    image: '',
+    pump_id: 6
+}
+
+const Ingredients = [VODKA, ORANGE_JUICE, TEQUILA, RUM, LIME_JUICE, COKE, CRANBERRY];
+
+export { Ingredients, VODKA, ORANGE_JUICE, TEQUILA, RUM, LIME_JUICE, COKE, CRANBERRY };

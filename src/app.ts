51f51b7acdd4a9701aca5d5040ab444a8fb88b97
JT Salisbury <crazyscouter1@gmail.com>
2022-08-12 12:55:18 -0400

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,12 @@ const port = process.env.PORT || 8080;
 
 app.use(cors());
 
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app.use(express.static('assets'))
 
 /**
